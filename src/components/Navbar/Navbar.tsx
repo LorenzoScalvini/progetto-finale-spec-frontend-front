1c88a6df131e107c4cd68bf56b3aca17f7f10c71
LorenzoScalvini <lorenzo.scalvini1704@gmail.com>
2025-05-16 09:12:42 +0200

    Modernizes navbar with CSS variables and icon integration
    Enhances maintainability and visual consistency by:
    - Replacing hardcoded values with CSS custom properties
    - Implementing HeroIcons for improved visual hierarchy
    - Streamlining hover animations and transitions
    - Standardizing spacing and color schemes
    
    Improves component reusability and alignment with design system standards while maintaining existing functionality.

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,6 +1,12 @@
 import React from 'react';
 import styles from './Navbar.module.css';
 import { Link } from 'react-router-dom';
+import { 
+  HomeIcon,
+  HeartIcon,
+  ScaleIcon,
+  ShoppingBagIcon 
+} from '@heroicons/react/24/outline';
 
 interface StarbucksNavbarProps {
   logoSrc: string;
@@ -12,7 +18,7 @@ interface StarbucksNavbarProps {
 
 const Navbar: React.FC<StarbucksNavbarProps> = ({
   logoSrc,
-  brandName = 'Starbucks',
+  brandName = ' STARBUCKS',
   altText = 'Starbucks Logo',
   buttonLabel = 'Ordina ora',
   onButtonClick
@@ -22,15 +28,27 @@ const Navbar: React.FC<StarbucksNavbarProps> = ({
       <Link to="/" className={styles.brand}>
         <div className={styles.logoContainer}>
           <img src={logoSrc} alt={altText} className={styles.logo} />
-        </div> {brandName}
+        </div>
       </Link>
 
       <div className={styles.actions}>
-        <Link to="/" className={styles.linkButton}>Home</Link>
-        <Link to="/coffeesfavorite" className={styles.linkButton}>Preferiti</Link>
-        <Link to="/coffeecomparator" className={styles.linkButton}>Confronta</Link>
+        <Link to="/" className={styles.linkButton}>
+          <HomeIcon className={styles.icon} />
+          Home
+        </Link>
+        
+        <Link to="/favorites" className={styles.linkButton}>
+          <HeartIcon className={styles.icon} />
+          Preferiti
+        </Link>
+        
+        <Link to="/coffeecomparator" className={styles.linkButton}>
+          <ScaleIcon className={styles.icon} />
+          Confronta
+        </Link>
 
         <button className={styles.button} onClick={onButtonClick}>
+          <ShoppingBagIcon className={styles.icon} />
           {buttonLabel}
         </button>
       </div>
